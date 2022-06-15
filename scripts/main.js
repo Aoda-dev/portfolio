2debf407d1fc2c6181163d6f36e896709df09b08
Tolegen <hinokamiaoda@gmail.com>
2022-06-15 21:22:50 +0600

    fixed bugs

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,7 @@ import { scrollInit } from './scroll'
 const className = '.app'
 
 const start = async () => {
+	await document.fonts.load('1rem "Montserrat')
 	await init(className)
 	await hero(className)
 	await scrollInit()
